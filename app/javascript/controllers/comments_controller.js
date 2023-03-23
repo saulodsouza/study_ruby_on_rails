364c206ada134a589c7849f75da42a7500b42964
Saulo d'Souza <saulosouza@proton.me>
2023-03-23 10:51:42 -0300

    add notification and cancel button on edit comment

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -6,15 +6,33 @@ export default class extends Controller {
   }
 
   toggleForm(event) {
-    console.log("Clicked");
     event.preventDefault();
     event.stopPropagation();
     const formId = event.params["form"];
     const commentBodyId = event.params["body"];
+    const editButtonId = event.params["edit"];
+
     const form = document.getElementById(formId);
+    const commentBody = document.getElementById(commentBodyId);
+    const editButton = document.getElementById(editButtonId);
+
     form.classList.toggle("d-none");
     form.classList.toggle("mt-5");
-    const commentBody = document.getElementById(commentBodyId);
     commentBody.classList.toggle("d-none");
+    this.toggleEditButton(editButton);
+  }
+
+  toggleEditButton(editButton) {
+    if (editButton.innerText == "Edit") {
+      editButton.innerText = "Cancel";
+    } else {
+      editButton.innerText = "Edit";
+    }
+    this.toggleEditButtonClass(editButton);
+  }
+
+  toggleEditButtonClass(editButton) {
+    editButton.classList.toggle("btn-secondary");
+    editButton.classList.toggle("btn-warning");
   }
 }
